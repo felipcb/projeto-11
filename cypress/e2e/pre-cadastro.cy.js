9a117282f80c7be37cff1a4a27a547338383991e
Felipe Barreto <felipbarreto@icloud.com>
2022-07-26 23:28:26 -0300

    pastas pre-cadastro e produto

--- cypress/e2e/pre-cadastro.cy.js
+++ cypress/e2e/pre-cadastro.cy.js
@@ -1,15 +1,27 @@
 /// <reference types = "cypress"/>
- 
+
+import { faker } from '@faker-js/faker';
+
 beforeEach(() => {
     cy.visit('http://lojaebac.ebaconline.art.br/')
     cy.get('.icon-user-unfollow').click()
-  });
+});
 
 describe('Usuário deve realizar o pre-cadastro', () => {
-    
+
     it('Cadastrar com sucesso', () => {
-        cy.get('#reg_email').
-        cy.get('#reg_password')
+        cy.get('#reg_email').type(faker.internet.email())
+        cy.get('#reg_password').type(faker.internet.password())
         cy.get(':nth-child(4) > .button').click()
+
+        cy.get('.woocommerce-MyAccount-navigation-link--edit-account > a').click()
+        cy.get('#account_first_name').type(faker.name.firstName())
+        cy.get('#account_last_name').type(faker.name.lastName())
+        cy.get('.woocommerce-Button').click()
+        
+        cy.get('.woocommerce-message').should('contain', 'Detalhes da conta modificados com sucesso.')
+
     });
+
+
 });
---
+++ cypress/e2e/produto.cy.js
@@ -0,0 +1,21 @@
+/// <reference types = "cypress"/>
+
+beforeEach(() => {
+    cy.visit('http://lojaebac.ebaconline.art.br/produtos/')
+});
+
+describe('Adicionar produto ao carrinho', () => {
+    
+    it('Deve adicionar produto ao carrinho', () => {
+        cy.get('[class="product-block grid"]')
+        .eq(5)
+        .click()
+        cy.get('.button-variable-item-XL').click()
+        cy.get('.button-variable-item-Yellow').click()
+        cy.get('.input-text').clear().type(2)
+        cy.get('.single_add_to_cart_button').click()
+        cy.get('.dropdown-toggle > .mini-cart-items').should('contain', '2')
+
+        
+    });
+});
